d612a43b0f55353daa305c2dc8ae6b6d279e8710
emon-webdev <emon.webdev@gamil.com>
2024-01-11 10:46:22 +0600

    udapte home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,20 @@
 import { Box } from "@mui/material";
 import Banner from "./Banner";
+import FeatureProfile from "./FeatureProfile";
+import HappyStories from "./HappyStories";
+import Membership from "./Membership";
+import Services from "./Services";
+import Testimonial from "./Testimonial";
 
 const Home = () => {
   return (
     <Box className="home-page">
       <Banner />
+      <HappyStories />
+      <FeatureProfile />
+      <Services />
+      <Membership />
+      <Testimonial />
     </Box>
   );
 };
