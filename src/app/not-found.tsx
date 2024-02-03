af9c55175448e97bae08a95b4f4579dd76f7ab0e
emon-webdev <emon.webdev@gamil.com>
2024-02-03 12:17:22 +0600

    update loading page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,36 @@
+import { Box, Container, Typography } from "@mui/material";
+import Link from "next/link";
+import errorBg from "../assets/images/error/error.png";
 const NotFoundPage = () => {
   return (
-    <div>
-      <h1>404 Page not found !</h1>
-    </div>
+    <Box
+      style={{
+        // background: "#ddd",
+        background: `url(${errorBg})`,
+        backgroundSize: "cover",
+        height: "calc(100vh - 480px)",
+        minHeight: "500px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        textAlign: "center",
+      }}
+    >
+      <Container maxWidth="xl">
+        <Typography variant="h1">
+          <Typography component="span">4</Typography>
+          <Typography component="span">0</Typography>
+          <Typography component="span">4</Typography>
+        </Typography>
+        <Typography variant="h3">Oops!</Typography>
+        <Typography>
+          We`re sorry,
+          <br /> The page you were looking for doesn`t exist anymore.
+        </Typography>
+        <Link href="/">Back To Home</Link>
+      </Container>
+    </Box>
   );
 };
 
